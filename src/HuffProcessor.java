f13cbb447c3c5a6384ad3b4181b7a4e4a99138e7
Griffin Malm <griffin.malm@duke.edu>
2018-12-08 14:05:23 -0500

    readTreeHeader done

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,7 +1,3 @@
-// lmaoooo yeEeeeT
-
-// hi griffin ;-)))))
-
 /**
  * Although this class has a history of several years,
  * it is starting from a blank-slate, new and clean implementation
@@ -85,6 +81,15 @@ public class HuffProcessor {
 
 	private HuffNode readTreeHeader(BitInputStream in) {
 		// TODO Auto-generated method stub
-		return null;
+		int bit = in.readBits(1);
+		if (bit == -1) throw new HuffException(-1 + " is not a legal bit");
+		if (bit == 0) {
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0, 0, left, right);
+		} else {
+			int value = in.readBits(BITS_PER_WORD + 1);
+			return new HuffNode(value, 0, null, null);
+		}
 	}
 }
